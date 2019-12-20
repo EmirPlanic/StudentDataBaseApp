b192b6dd016a0cd10a0c0a3b9d6b12ab73249840
Emir Planic <plana799@gmail.com>
2019-12-20 17:19:08 +0100

    Made set method for ID

--- src/studentDataBaseApp/Student.java
+++ src/studentDataBaseApp/Student.java
@@ -7,11 +7,11 @@ public class Student {
  private String firstName;
  private String lastName;
  private int gradeYear;
- private int studentID;
+ private String studentID;
  private String courses;
  private int tuitionBalance;
- private int costOfCourse = 600;
-	
+ private static int costOfCourse = 600;
+ private static int id = 1000;	
  
  public Student () {
 	 Scanner input = new Scanner (System.in);
@@ -22,10 +22,24 @@ public class Student {
 	 System.out.println("Enter student last name");
 	 this.lastName = input.nextLine();
 	 
-	 System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 -Senior\nEnter student grade level");
+	 System.out.println("1 - Freshmen\n2 - Sophmore\n3 - Junior\n4 - Senior\nEnter student grade level");
 	 this.gradeYear = input.nextInt();
 	 
-	 System.out.println(firstName + " " + lastName + " " + gradeYear);
+	 setStudentID();
+	 
+	 System.out.println(firstName + " " + lastName + " " + gradeYear + " " + studentID);
+	 
+	
+	 
  }
+ 
+ private void  setStudentID() {
+	 id++;
+	 this.studentID = gradeYear + "" + id;
+	 
+ }
+ 
+ 
+ 
 	
 }
